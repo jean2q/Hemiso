fa85e91f8f7130306b847aadd2263e17ee7ef9d4
Jean Lima <pdelima.jeanlucas@gmail.com>
2020-11-09 17:51:01 -0300

    geraLaudo.js não está salvando a session storage na variavel dados

--- geraLaudo.js
+++ geraLaudo.js
@@ -1,7 +1,6 @@
 
     var dados = window.sessionStorage
 
-
     //DADOS PACIENTE
 
     function preencher (dados){ //preenche dados de campos com o mesmo ID no formulário e no laudo
@@ -50,6 +49,7 @@
 
 
     // RELACIONA VALORES DE REFERÊNCIA COM IDADE E SEXO DO PACIENTE
+    if (dados.tipoIdade == 'anos'){
     if (dados.idade <= 12 && dados.idade >= 6) //se for menor ou igual que 12 anos e maior ou igual 6
     {
         var valorRef = {
@@ -88,7 +88,7 @@
             refPlaq: {min: 200, max: 450},
         }
     }
-    else if (dados.idade < 2) //sse for menor do que 6 e maior ou igual a 1 ano
+    else if (dados.idade < 2)
     {
         var valorRef = {
             ID: 'menor que 2 anos',
@@ -146,6 +146,144 @@
             refBaso: {min: 0.02, max: 0.1},
             refPlaq: {min: 150, max: 400},
         }
+    }}
+
+    else if (dados.tipoIdade == "meses") {
+
+        if (dados.idade <= 1) //meses de 0 a 1
+        {
+            var valorRef = {
+                ID: '0 A 1 MESES',//NÃO MUDEI OS VALORES ABAIXO
+                refHemacias: {min: 3, max: 5.4}, //preenchido
+                refHb: {min: 11.5, max: 16.5},
+                refHt: {min: 33, max: 53}, 
+                refVCM: {min: 92, max: 116},
+                refHCM: {min: 30, max: 36},
+                refCHCM: {min: 29, max: 37}, 
+                refLeuc: {min: 5, max: 19},
+                refNeut: {min: 3, max:9},
+                refLinf: {min: 3, max: 16},
+                refMono: {min: 0.3, max: 1},
+                refEosi: {min: 0.2, max: 1}, 
+                refBaso: {min: 0.02, max: 0.1},//TODOS IGUAIS
+                refPlaq: {min: 210, max: 650},
+            }
+        }
+        else if (dados.idade == 2) //meses 2
+        {
+            var valorRef = {
+                ID: '2 MESES',//PREENCHIDO
+                refHemacias: {min: 3.1, max: 4.3},
+                refHb: {min: 9.4, max: 13},
+                refHt: {min: 28, max: 42}, 
+                refVCM: {min: 87, max: 103},
+                refHCM: {min: 27, max: 33},
+                refCHCM: {min: 27, max: 37}, 
+                refLeuc: {min: 5, max: 15},
+                refNeut: {min: 1, max: 5},
+                refLinf: {min: 4, max: 10},
+                refMono: {min: 0.4, max: 1.2},
+                refEosi: {min: 0.1, max: 1},
+                refBaso: {min: 0.02, max: 0.1},//TODOS IGUAIS
+                refPlaq: {min: 210, max: 650},
+            }
+        }
+        else if (dados.idade >= 3 && dados.idade < 11) //meses  MAIOR IGUAL A 3 OU MENOR QUE 11
+        {
+            var valorRef = {
+                ID: '3 a 11 meses',//preenchido
+                refHemacias: {min: 4.1, max: 5.3},
+                refHb: {min: 11.1, max: 14.1},
+                refHt: {min: 30, max: 40}, 
+                refVCM: {min: 68, max: 84},
+                refHCM: {min: 24, max: 30},
+                refCHCM: {min: 30, max: 36}, 
+                refLeuc: {min: 6, max: 18},
+                refNeut: {min: 1, max:6},
+                refLinf: {min: 4, max: 12},
+                refMono: {min: 0.2, max: 1.2},
+                refEosi: {min: 0.1, max: 1},
+                refBaso: {min: 0.02, max: 0.1},//TODOS IGUAIS
+                refPlaq: {min: 200, max: 550},
+            }
+        }
+        else if (dados.idade >= 12 && dados.idade < 23) //DE 12 A 23 MESES
+        {
+            var valorRef = {
+                ID: 'entre 1 ano e 2', //PREENCHIDO ok
+                refHemacias: {min: 3.9, max: 5.1}, 
+                refHb: {min: 11.1, max: 14.1},
+                refHt: {min: 30, max: 38}, 
+                refVCM: {min: 72, max: 84},
+                refHCM: {min: 24, max: 30},
+                refCHCM: {min: 31, max: 37}, 
+                refLeuc: {min: 6, max: 16},
+                refNeut: {min: 1, max:7},
+                refLinf: {min: 3.5, max: 11},
+                refMono: {min: 0.2, max: 1},
+                refEosi: {min: 0.1, max: 1},
+                refBaso: {min: 0.02, max: 0.1},//TODOS IGUAIS
+                refPlaq: {min: 200, max: 550},
+            }
+        }
+    }
+    else if (dados.tipoIdade == "dias"){
+        if (dados.idade <= 2) //idade menor que 2 dias
+        {
+            var valorRef = {
+                ID: 'RECEM NASCIDO', //preenchido
+                refHemacias: {min: 5, max: 7},
+                refHb: {min: 14, max: 18},
+                refHt: {min: 45, max: 75}, 
+                refVCM: {min: 100, max: 120},
+                refHCM: {min: 31, max: 37},
+                refCHCM: {min: 30, max: 36}, 
+                refLeuc: {min: 10, max: 26},
+                refNeut: {min: 4, max:14},
+                refLinf: {min: 3, max: 8},
+                refMono: {min: 0.5, max: 2},
+                refEosi: {min: 0.1, max: 1},
+                refBaso: {min: 0.02, max: 0.1},//TODOS IGUAIS
+                refPlaq: {min: 150, max: 450},
+            }
+        }
+        else if (dados.idade >= 3 && dados.idade <= 29) //de 3 a 29 dias
+        {
+            var valorRef = {
+                ID: 'de 3 dias a 29', //não mudei ainda
+                refHemacias: {min: 3.9, max: 5.1},
+                refHb: {min: 11, max: 14},
+                refHt: {min: 35, max: 45}, 
+                refVCM: {min: 77, max: 95},
+                refHCM: {min: 25, max: 33},
+                refCHCM: {min: 31, max: 37}, 
+                refLeuc: {min: 5, max: 13},
+                refNeut: {min: 2, max:8},
+                refLinf: {min: 1, max: 5},
+                refMono: {min: 0.2, max: 1},
+                refEosi: {min: 0.1, max: 1},
+                refBaso: {min: 0.02, max: 0.1},//TODOS IGUAIS
+                refPlaq: {min: 180, max: 400},
+            }
+        }
+        else if (dados.idade >= 30 && dados.idade <= 60) //de 1 a 2 meses
+        {
+            var valorRef = {
+                ID: '0 A 1 MESES',//NÃO MUDEI OS VALORES ABAIXO
+                refHemacias: {min: 3, max: 5.4}, //preenchido
+                refHb: {min: 11.6, max: 16.5},
+                refHt: {min: 33, max: 53}, 
+                refVCM: {min: 92, max: 116},
+                refHCM: {min: 30, max: 36},
+                refCHCM: {min: 29, max: 37}, 
+                refLeuc: {min: 5, max: 19},
+                refNeut: {min: 1, max:6},
+                refLinf: {min: 3, max: 16},
+                refMono: {min: 0.3, max: 1},
+                refEosi: {min: 0.2, max: 1}, 
+                refBaso: {min: 0.02, max: 0.1},//TODOS IGUAIS
+                refPlaq: {min: 210, max: 650},
+            }
     }
 
   
